c76c3163d254f5f480ed9d32ca415101160d24fc
lucyimhoff <lucyi4@nycstudents.net>
2024-09-19 09:34:08 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,8 +11,15 @@ public class Main
        System.out.println(g.getCurrentFood());
        //int random = (int) (Math.random() * 50 + 1);
        //System.out.println(random);
-       System.out.println();
        System.out.println(Math.random() < 0.05);
+       g.simulateOneDay(12);
+       System.out.println(g.getCurrentFood());
+       Feeder h = new Feeder(1000);
+       h.simulateOneDay(22);
+       System.out.println(h.getCurrentFood());
+       Feeder i = new Feeder(100);
+       i.simulateOneDay(5);
+       System.out.println(i);
     }
 
 }
